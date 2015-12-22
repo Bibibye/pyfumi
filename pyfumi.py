2477c72ee6f6e9a79de5747cd95e357c7f982179
Bibibye <Bibibye@users.noreply.github.com>
2015-12-22 17:57:52 +0100

    mixed / markov comparison

--- pyfumi.py
+++ pyfumi.py
@@ -4,9 +4,12 @@ from game.game import Game
 from strats import *
 
 if __name__ == "__main__":
-    player_1 = dummy.Dummy()
+    player_1 = mixed.Mixed()
     player_2 = markov.Markov()
     print(player_1.get_name()+" vs "+player_2.get_name())
     g = Game(player_1, player_2)
-    r = g.run_n(100)
-    print(r)
+    r = g.run_n(1000)
+    print("results :"+
+          "\n\tDraw : "+str(r.count(0))+
+          "\n\t"+player_1.get_name()+" : "+str(r.count(1))+
+          "\n\t"+player_2.get_name()+" : "+str(r.count(-1)))
