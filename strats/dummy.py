c2bd820a1afcf0fc91426e6315d5a4d7690fdb74
Bibibye <Bibibye@users.noreply.github.com>
2015-12-22 18:22:26 +0100

    dummy++

--- strats/dummy.py
+++ strats/dummy.py
@@ -1,12 +1,16 @@
 from strats.strat import Strat
 
 class Dummy(Strat):
-    def __init__(self, choice='R'):
+    def __init__(self, choice=['R']):
         self.choice = choice
         self.name = "dummy"
+        self.count = 0
+        self.frequency = len(choice)
 
     def play(self):
-        return self.choice
+        choice = self.choice[self.count%self.frequency]
+        self.count += 1
+        return choice
 
     def update(self, opponent_last_play):
         pass
